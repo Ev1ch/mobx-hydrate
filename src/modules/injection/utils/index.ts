0f29694bc1de98bf294f3f967bc1724f00c75a14
Vlad Zusko <vladzvu11@gmail.com>
2023-06-25 01:18:45 +0300

    fix(modules/injection): fix type imports in utils index

--- src/modules/injection/utils/index.ts
+++ src/modules/injection/utils/index.ts
@@ -3,14 +3,8 @@ export {
   default as createResultWithInjectionGetter,
 } from './createResultWithInjectionGetter';
 export {
-  type GetResultWithInjection,
-  default as getResultWithInjection,
-} from './getResultWithInjection';
-export {
   type CreateInjectionFromResultGetter,
   default as createInjectionFromResultGetter,
 } from './createInjectionFromResultGetter';
-export {
-  type GetInjectionFromResult,
-  default as getInjectionFromResult,
-} from './getInjectionFromResult';
+export { default as getResultWithInjection } from './getResultWithInjection';
+export { default as getInjectionFromResult } from './getInjectionFromResult';
