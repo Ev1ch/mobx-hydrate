ec8ef85097e12d29c841fb22a31556dc051d26c6
Vlad Zusko <vladzvu11@gmail.com>
2023-06-26 23:31:47 +0300

    fix(utils): simplify returning in createStores

--- src/utils/createStores.ts
+++ src/utils/createStores.ts
@@ -2,11 +2,10 @@ import type { ConstructedStores, Stores } from '@/domain';
 
 export type CreateStores = <TStores extends Stores>(stores: TStores) => ConstructedStores<TStores>;
 
-const createStores: CreateStores = <TStores extends Stores>(stores: TStores) => {
-  return Object.entries(stores).reduce((accumulator, [key, Store]) => {
+const createStores: CreateStores = <TStores extends Stores>(stores: TStores) =>
+  Object.entries(stores).reduce((accumulator, [key, Store]) => {
     accumulator[key as keyof TStores] = new Store();
     return accumulator;
   }, {} as ConstructedStores<TStores>);
-};
 
 export default createStores;
