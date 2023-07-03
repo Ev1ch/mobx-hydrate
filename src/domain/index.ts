cab9302cbfb5e8e6901e931779fb077917c5bab6
Vlad Zusko <vladzvu11@gmail.com>
2023-07-03 10:32:06 +0300

    feat(domain): add some root types

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,6 +1,10 @@
 import type { Constructor } from '@/utils';
 
-export type Stores = Record<string, Constructor>;
+export type Store = Constructor;
+
+export type Stores = Record<string, Store>;
+
+export type ConstructedStore<TStore extends Store> = InstanceType<TStore>;
 
 export type ConstructedStores<TStores extends Stores> = {
   [key in keyof TStores]: InstanceType<TStores[key]>;
