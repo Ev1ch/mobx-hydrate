b9bb604f49f5b92f788648f0eb068b3a2aad0819
Vlad Zusko <vladzvu11@gmail.com>
2023-07-04 22:40:12 +0300

    feat(schemas): add stores schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,2 +1,3 @@
+export { default as StoresSchema } from './StoresSchema';
 export { default as OptionsSchema } from './OptionsSchema';
 export { type Validate, default as validate } from './validate';
