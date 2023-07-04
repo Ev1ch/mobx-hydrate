3c25ab217b558b4197eed2b3a895ac16bffc4f6d
Vlad Zusko <vladzvu11@gmail.com>
2023-07-04 22:44:39 +0300

    feat(domain): add root options

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,4 +1,8 @@
 import type { Constructor } from '@/utils';
+import type { SerializationOptions } from '@/modules/serialization';
+import type { InjectionOptions } from '@/modules/injection';
+import type { StoredServerPropsGetter } from '@/modules/server-props';
+import type { HydratableStores, UseHydratedStores } from '@/modules/hydration';
 
 export type Store = Constructor;
 
@@ -8,4 +12,21 @@ export type ConstructedStore<TStore extends Store> = InstanceType<TStore>;
 
 export type ConstructedStores<TStores extends Stores> = {
   [key in keyof TStores]: InstanceType<TStores[key]>;
+};
+
+export interface Options {
+  serialization?: Partial<SerializationOptions>;
+  injection?: Partial<InjectionOptions>;
+}
+
+export type ProvidedOptions = {
+  [key in keyof Options]-?: Required<Options[key]>;
+};
+
+export type CreateWithStores = <TStores extends HydratableStores>(
+  stores: TStores,
+  options: Options,
+) => {
+  withStores: StoredServerPropsGetter<TStores>;
+  useHydratedStores: UseHydratedStores<TStores>;
 };
