dbb8a584eebec512f301e292c38c2596c6aefb59
Vlad Zusko <vladzvu11@gmail.com>
2023-07-04 22:42:48 +0300

    feat(modules/injection): add default options

--- src/modules/injection/constants.ts
+++ src/modules/injection/constants.ts
@@ -1,1 +1,7 @@
+import type { InjectionOptions } from './domain';
+
 export const HYDRATION_KEY: string = '__MOBX_HYDRATED_STORES__';
+
+export const DEFAULT_INJECTION_OPTIONS: InjectionOptions = {
+  key: HYDRATION_KEY,
+};
